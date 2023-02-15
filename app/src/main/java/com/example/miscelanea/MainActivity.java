6a0eb7bd948c0a36229d289c4e007d9df02d4a6d
Danikileitor <focusyi@hotmail.com>
2023-02-15 09:06:10 +0100

    Miscelanea

--- app/src/main/java/com/example/miscelanea/MainActivity.java
+++ app/src/main/java/com/example/miscelanea/MainActivity.java
@@ -3,12 +3,62 @@ package com.example.miscelanea;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public EditText txtOperando1, txtOperando2;
+    public TextView lbResultado;
+    public String valorInicial;
+    public int operando1, operando2, resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtOperando1 = findViewById(R.id.txtOperando1);
+        txtOperando2 = findViewById(R.id.txtOperando2);
+        lbResultado = findViewById(R.id.lbResultado);
+        valorInicial = getString(R.string.lbResultado);
+    }
+
+    public void potencia(View view) {
+        if (!String.valueOf(txtOperando1.getText()).matches("")){
+            operando1 = Integer.parseInt(String.valueOf(txtOperando1.getText()));
+        }else{operando1 = 0;}
+        if (!String.valueOf(txtOperando2.getText()).matches("")){
+            operando2 = Integer.parseInt(String.valueOf(txtOperando2.getText()));
+        }else{operando2 = 0;}
+        resultado = (int) Math.pow(operando1, operando2);
+        lbResultado.setText(String.valueOf(resultado));
+    }
+
+    public void factorial(View view) {
+        if (!String.valueOf(txtOperando1.getText()).matches("")){
+            operando1 = Integer.parseInt(String.valueOf(txtOperando1.getText()));
+        }else{operando1 = 0;}
+        resultado = 1;
+        for (int i=operando1;i>0;i--)
+        {
+            resultado=resultado*i;
+        }
+        lbResultado.setText(String.valueOf(resultado));
+    }
+
+    public void fibonacci(View view) {
+        if (!String.valueOf(txtOperando1.getText()).matches("")){
+            operando1 = Integer.parseInt(String.valueOf(txtOperando1.getText()));
+        }else{operando1 = 0;}
+        String fibonacci = "";
+        int num1 = 0, num2 = 1, suma = 1;
+        for (int i = 1; i < operando1; i++) {
+            fibonacci += suma + "/";
+            suma = num1 + num2;
+            num1 = num2;
+            num2 = suma;
+        }
+        fibonacci = fibonacci.substring(0, fibonacci.length()-1);
+        lbResultado.setText(fibonacci);
     }
 }
